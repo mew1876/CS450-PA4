22c226b7593aa8746c9fea8762d482e94502e241
Mike <mew1876@gmail.com>
2018-04-23 13:31:48 -0500

    added fschecker skeleton

---
+++ xv6-public/fsChecker.c
@@ -0,0 +1,16 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fs.h"
+
+void getAllocedINodes(int *table, int ninodes) {
+
+}
+
+void getReachableINodes(int *table, int ninodes) {
+
+}
+
+int main(int argc, char *argv[]) {
+
+}
